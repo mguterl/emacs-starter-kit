58d7ac5f7ce1e804e518b88b2bb5727993cab023
Michael Guterl <mguterl@gmail.com>
2010-05-09 10:17:30 -0400

    basic customizations

--- michaelguterl.el
+++ michaelguterl.el
@@ -1,3 +1,58 @@
+(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
+
+;; Save backups in one place
+;; Put autosave files (ie #foo#) in one place, *not*
+;; scattered all over the file system!
+(defvar autosave-dir
+  (concat "/tmp/emacs_autosaves/" (user-login-name) "/"))
+
+(make-directory autosave-dir t)
+
+(defun auto-save-file-name-p (filename)
+  (string-match "^#.*#$" (file-name-nondirectory filename)))
+
+(defun make-auto-save-file-name ()
+  (concat autosave-dir
+          (if buffer-file-name
+              (concat "#" (file-name-nondirectory buffer-file-name) "#")
+            (expand-file-name
+             (concat "#%" (buffer-name) "#")))))
+
+;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
+;; list contains regexp=>directory mappings; filenames matching a regexp are
+;; backed up in the corresponding directory. Emacs will mkdir it if necessary.)
+(defvar backup-dir (concat "/tmp/emacs_backups/" (user-login-name) "/"))
+(setq backup-directory-alist (list (cons "." backup-dir)))
+
+;; Full screen toggle
+(defun toggle-fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                         'fullboth)))
+
+(global-set-key (kbd "M-n") 'toggle-fullscreen)
+
+;; start server for emacsclient
+(server-start)
+
+;; misc
+(prefer-coding-system 'utf-8)
+
+;; map command as meta
+(setq ns-command-modifier (quote meta))
+
+;; cleanup trailing whitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; while I <square box> Unicode as much as the next guy,
+;; I want my lambdas left alone.
+(remove-hook 'coding-hook 'pretty-lambdas)
+
+;; just nice to have everywhere
+(add-hook 'coding-hook (lambda () (setq tab-width 2)))
+
+;; cucumber support
 (add-to-list 'load-path (concat dotfiles-dir "vendor/cucumber"))
 (require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
