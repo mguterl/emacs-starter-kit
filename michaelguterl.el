d14c33f53a3e6d249e7be6606a5fdb332e9145b2
Michael Guterl <mguterl@gmail.com>
2011-03-08 18:36:14 -0500

    set coffee-command, indentation

--- michaelguterl.el
+++ michaelguterl.el
@@ -146,10 +146,12 @@
   ;; Emacs key binding
   (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
 
+  (setq coffee-command "/usr/local/share/npm/bin/coffee")
+
   ;; Compile '.coffee' files on every save
   (add-hook 'after-save-hook
-      '(lambda ()
-         (when (string-match "\.coffee$" (buffer-name))
-          (coffee-compile-file)))))
+            '(lambda ()
+               (when (string-match "\.coffee$" (buffer-name))
+                 (coffee-compile-file)))))
 
 (add-hook 'coffee-mode-hook '(lambda () (coffee-custom)))
